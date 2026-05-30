e17bc74b44ef79cb37eda2b7fe6aab40dfa8a505
emmasacchetti04 <emma.sacchetti@mail.polimi.it>
2026-05-30 11:35:55 +0200

    Update istogramma.cpp

--- istogramma.cpp
+++ istogramma.cpp
@@ -7,9 +7,7 @@
 
 int main() {
     double start_time = omp_get_wtime();
-    int NUM_THREADS = 1; 
-    omp_set_num_threads(NUM_THREADS);
-
+   int NUM_THREADS = omp_get_max_threads();
 
     // Generazione stringa di input
     char *stringa = (char *)malloc(LENGTH * sizeof(char));
