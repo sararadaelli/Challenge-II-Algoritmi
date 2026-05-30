f73f38e4718dc07aa751a7da76ad0a58dac2755a
sararadaelli <sara.radaelli@mail.polimi.it>
2026-05-30 11:59:22 +0200

    Update istogramma.cpp

--- istogramma.cpp
+++ istogramma.cpp
@@ -6,14 +6,15 @@
 #define LENGTH 1000000
 
 int main() {
-    int NUM_THREADS = omp_get_max_threads(); // legge la variabile che verrà impostata dall'utente nel terminale (1,2,8,16,...)
+    int NUM_THREADS = omp_get_max_threads();
 
     // Generazione stringa di input
-    char *stringa = (char *)malloc(LENGTH * sizeof(char));
+    char *data = (char *)malloc(LENGTH * sizeof(char));
     srand(42); // seme fisso per riproducibilità
     for (int i = 0; i < LENGTH; i++) {
-        stringa[i] = 'a' + (rand() % 26);
+        data[i] = 'a' + (rand() % 26);
     }
+    
     double start_time = omp_get_wtime();
     
     // Inizializzazione
@@ -21,22 +22,22 @@ int main() {
     int local_histograms[NUMERO_BINS][NUM_THREADS];
 
     for (int b = 0; b < NUMERO_BINS; b++) {
-        histogram[b] = 0; // histogram[0] = 0, lettere a b c d, etc.
-        for (int thr = 0; thr < NUM_THREADS; thr++) // 
-            local_histograms[b][thr] = 0; // thread per ogni contatore: local_histograms[0][0] = 0  → bin 0, thread 0, etc.
+        histogram[b] = 0;
+        for (int thr = 0; thr < NUM_THREADS; thr++)
+            local_histograms[b][thr] = 0;
     }
 
     // Regione parallela
     #pragma omp parallel
     {
-        int tid = omp_get_thread_num(); // ID del thread: 0, 1, 2, ...
+        int tid = omp_get_thread_num();
 
         #pragma omp for
         for (int i = 0; i < LENGTH; ++i) { 
-            int alphabet_pos = (int)stringa[i] - 'a'; // conversione da carattere a numero
-            if (alphabet_pos >= 0 && alphabet_pos < 26) { // check che sia lettera minuscola (esclude altri caratteri)
-                int bin = alphabet_pos / 4; // divisione nell'apposito bin
-                local_histograms[bin][tid]++; // incrementa il contatore privato del thread (tid identifica)
+            int alphabet_pos = (int)data[i] - 'a';
+            if (alphabet_pos >= 0 && alphabet_pos < 26) {
+                int bin = alphabet_pos / 4;
+                local_histograms[bin][tid]++;
             }
         }
     }
@@ -51,8 +52,8 @@ int main() {
 
     // Stampa istogramma
     // for (int b = 0; b < NUMERO_BINS; b++)
-        // printf("bin[%d]: %d\n", b, histogram[b]);
+    //     printf("bin[%d]: %d\n", b, histogram[b]);
 
-    free(stringa);
+    free(data);
     return 0;
 }
