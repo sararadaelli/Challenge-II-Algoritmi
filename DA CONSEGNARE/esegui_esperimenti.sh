453475a10d682fd8d4a856e35187f5f5a368f07e
emmasacchetti04 <emma.sacchetti@mail.polimi.it>
2026-06-03 10:41:42 +0200

    Update esegui_esperimenti.sh

--- DA CONSEGNARE/esegui_esperimenti.sh
+++ DA CONSEGNARE/esegui_esperimenti.sh
@@ -1,3 +1,10 @@
+#PER L'ESECUZIONE DEL CODICE: questo file è uno script che serve per compilare ed eseguire i codici. 
+#Per avviarlo basta eseguire questo file dal terminale.
+#NOTA SULLA RIPRODUCIBILITA': invece di fissare il numero di thread direttamente nel codice (ad esempio tramite
+#una #define), si è scelto di far scegliere all’utente il numero di threads al momento
+#dell’esecuzione, tramite la variabile OMP_NUM_THREADS. Questo garantisce
+#maggiore riproducibilità: utenti con numero di core diversi otterranno risultati coerenti
+#con la propria macchina.
 gcc-15 -fopenmp -o istogramma istogramma.cpp
 
 // IN ENTRAMBI I CASI L'UTENTE PUÒ SCEGLIERE IL NUMERO DI THREADS CON CUI ESEGUIRE IL PROGRAMMA
